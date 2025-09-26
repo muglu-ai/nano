008a827a32908236800cc92038249ac2c4c56a89
muglu <manishk_sharma@outlook.com>
2025-09-26 16:20:36 +0530

    directory form preview added number update

--- resources/views/exhibitor_info/form.blade.php
+++ resources/views/exhibitor_info/form.blade.php
@@ -327,10 +327,10 @@
                 iti = window.intlTelInput(phoneInput, {
                     initialCountry: "auto",
                     utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
-                    geoIpLookup: function (callback) {
+                    geoIpLookup: function(callback) {
                         fetch('https://ipapi.co/json')
                             .then(res => res.json())
-                            .then data => callback(data.country_code))
+                            .then(data => callback(data.country_code))
                             .catch(() => callback('IN'));
                     },
                     separateDialCode: true,
