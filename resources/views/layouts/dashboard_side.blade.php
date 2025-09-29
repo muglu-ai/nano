f6877e5da6c99f43379ba064542331a14a4c31e0
muglu <manishk_sharma@outlook.com>
2025-09-29 15:01:39 +0530

    list page update

--- resources/views/layouts/dashboard_side.blade.php
+++ resources/views/layouts/dashboard_side.blade.php
@@ -9,28 +9,30 @@
 
     <link rel="icon" href="https://www.bengalurutechsummit.com/favicon-16x16.png" type="image/vnd.microsoft.icon" />
     <title>
-        @yield('title' , 'SEMICON India 2025 Registration ')
+        {{ config('constants.EVENT_NAME') . ' ' . config('constants.EVENT_YEAR') . ' Admin Panel' }}
     </title>
-     <!--     Fonts and icons     -->
+    <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
-    <!-- Nucleo Icons -->
-    <link href="/asset/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="/asset/css/nucleo-svg.css" rel="stylesheet" />
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <!-- Material Icons -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
     <!-- CSS Files -->
-    <link id="pagestyle" href="/asset/css/material-dashboard.min.css?v=3.1.0" rel="stylesheet" />
+    <link id="pagestyle" href="{{ asset('asset/css/material-dashboard.min.css?v=3.1.0') }}" rel="stylesheet" />
+    <link id="pagestyle" href="{{ asset('public/asset/css/material-dashboard.min.css?v=3.1.0') }}" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.min.css" />
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}" />
+    <link rel="stylesheet" href="{{ asset('public/assets/css/custom.css') }}" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!-- Anti-flicker snippet (recommended)  -->
     <style>
         .async-hide {
             opacity: 0 !important
         }
     </style>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
 <body class="g-sidenav-show  bg-gray-100">
@@ -45,13 +47,19 @@
 </main>
 
 <!--   Core JS Files   -->
-<script src="/asset/js/core/popper.min.js"></script>
-<script src="/asset/js/core/bootstrap.min.js"></script>
-<script src="/asset/js/plugins/perfect-scrollbar.min.js"></script>
-<script src="/asset/js/plugins/smooth-scrollbar.min.js"></script>
+<script src="{{ asset('asset/js/core/popper.min.js') }}"></script>
+<script src="{{ asset('public/asset/js/core/popper.min.js') }}"></script>
+<script src="{{ asset('asset/js/core/bootstrap.min.js') }}"></script>
+<script src="{{ asset('public/asset/js/core/bootstrap.min.js') }}"></script>
+<script src="{{ asset('asset/js/plugins/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ asset('public/asset/js/plugins/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ asset('asset/js/plugins/smooth-scrollbar.min.js') }}"></script>
+<script src="{{ asset('public/asset/js/plugins/smooth-scrollbar.min.js') }}"></script>
 <!-- Kanban scripts -->
-<script src="/asset/js/plugins/dragula/dragula.min.js"></script>
-<script src="/asset/js/plugins/jkanban/jkanban.min.js"></script>
+<script src="{{ asset('asset/js/plugins/dragula/dragula.min.js') }}"></script>
+<script src="{{ asset('public/asset/js/plugins/dragula/dragula.min.js') }}"></script>
+<script src="{{ asset('asset/js/plugins/jkanban/jkanban.min.js') }}"></script>
+<script src="{{ asset('public/asset/js/plugins/jkanban/jkanban.min.js') }}"></script>
 
 
 
@@ -67,7 +75,10 @@
 <!-- Github buttons -->
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-<script src="/asset/js/material-dashboard.min.js?v=3.1.0"></script>
+<script src="{{ asset('asset/js/core/datatables.js') }}"></script>
+<script src="{{ asset('public/asset/js/core/datatables.js') }}"></script>
+
+
 </body>
 
 </html>
