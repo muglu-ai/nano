af1299feb56bc1f12097e221ab399d2144182b33
Manish Kumar Sharma <manishk_sharma@outlook.com>
2025-10-31 07:34:55 +0530

    emailFix

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1286,7 +1286,7 @@ class AdminController extends Controller
             $username = $application->user->email;
             $password = $application->user->simplePass;
             // echo $name . " - " . $username . " - " . $password . "<br>";
-            echo view('emails.credentials', ['email' => $username, 'name' => $name, 'password' => $password])->render();
+            echo view('emails.credentials', ['setupProfileUrl' => $setupProfileUrl, 'email' => $username, 'name' => $name, 'password' => $password])->render();
             exit;
             // exit;
             try {
