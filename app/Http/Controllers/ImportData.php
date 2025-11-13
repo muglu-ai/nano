006185e3f2b2f516d2b22049b411544b5dc236c0
Manish Kumar Sharma <manishk_sharma@outlook.com>
2025-11-13 16:29:55 +0530

    importFix

--- app/Http/Controllers/ImportData.php
+++ app/Http/Controllers/ImportData.php
@@ -45,7 +45,7 @@ class ImportData extends Controller
 
         // This query selects all columns from the exhibitors payments table (it_2025_exhibitors_dir_payment_tbl) where the pay_status is 'PAID'.
         // It retrieves a maximum of 50 records, skipping the very first (OFFSET 1).
-        $query = "SELECT * FROM it_2025_exhibitors_dir_payment_tbl WHERE pay_status = 'Paid'";
+        $query = "SELECT * FROM it_2025_exhibitors_dir_payment_tbl WHERE pay_status = 'Paid' and portalAccess = 0";
 
         // OR pay_status = 'Complimentary'
         
