d440aad4626ebd590071268631511cf226c92051
Manish Kumar Sharma <manishk_sharma@outlook.com>
2025-11-19 15:13:33 +0530

    pathCorrection

--- resources/views/e-visitor-guide/index.blade.php
+++ resources/views/e-visitor-guide/index.blade.php
@@ -17,8 +17,8 @@
         crossorigin="anonymous">
 
     <!-- Flipbook StyleSheets -->
-    <link href="{{ asset('public/dflip/css/dflip.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('public/dflip/css/themify-icons.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('dflip/css/dflip.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('dflip/css/themify-icons.min.css') }}" rel="stylesheet" type="text/css">
 
     <style>
         body {
@@ -92,8 +92,8 @@
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
-    <script src="{{ asset('public/dflip/js/libs/jquery.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('public/dflip/js/dflip.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('dflip/js/libs/jquery.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('dflip/js/dflip.min.js') }}" type="text/javascript"></script>
     <script>
         $(document).ready(function () {
             $('#flipbok_example').flipBook({
